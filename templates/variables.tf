c98684c6d25533414cac036c62820d2182680f87
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-11 13:28:47 +0530

    Update variables.tf

--- templates/variables.tf
+++ templates/variables.tf
@@ -1,2 +1,19 @@
 variable "ami_id" {
-  default = 
+  default = "ami-053b0d53c279acc90"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "ec2_instance"
+}
+
+variable "bucket_name" {
+  default = "unique_s3_bucket_1"
+}
+
+variable "table_name" {
+  default = "mradul_table"
+}
