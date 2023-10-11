79db6d6825cf4a856b72558b3179f6bc8e3e35c1
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-11 12:08:07 -0500

    Update dynamodb.tf

--- templates/dynamodb.tf
+++ templates/dynamodb.tf
@@ -1,9 +1,9 @@
-resources "aws_dynamodb_table" "my_table" {
+resource "aws_dynamodb_table" "my_table" {
   name = var.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "emailID"
 
-  attributes {
+  attributes = {
     name = "emailID"
     type = "S"
   }
