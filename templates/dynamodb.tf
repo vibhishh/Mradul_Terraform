7bcde60a07c38cc05d5b46beb775a8dcaca74741
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-11 13:15:52 +0530

    Create dynamodb.tf

---
+++ templates/dynamodb.tf
@@ -0,0 +1,10 @@
+resources "aws_dynamodb_table" "my_table" {
+  name = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "emailID"
+
+  attributes {
+    name = "emailID"
+    type = "S"
+  }
+}
