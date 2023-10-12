ecda32d4d094ed07334376f1e6346654126d99a5
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-12 01:12:33 -0500

    Create dynamodb.tf

---
+++ Terraform_Project/modules/files/dynamodb.tf
@@ -0,0 +1,10 @@
+resource "aws_dynamodb_table" "my_table" {
+  name = "${var.env_name}-${var.table_name}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "emailID"
+
+  attribute {
+    name = "emailID"
+    type = "S"
+  }
+}
