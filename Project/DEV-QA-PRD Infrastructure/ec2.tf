0dd5640b244e27d76b6ef006c12fc064eb9b86d8
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-12 00:57:14 -0500

    Delete Project/DEV-QA-PRD Infrastructure directory

--- Project/DEV-QA-PRD Infrastructure/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "my_ec2" {
-  count = 2
-  ami = var.ami_id
-  instance_type = var.instance_type
-  tags = {
-    name = "${var.env_name}-${var.instance_name}"
-    }
-}
--- Project/DEV-QA-PRD Infrastructure/s3_bucket.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "my_bucket" {
-    bucket = "${var.env_name}-${var.bucket_name}"
-}
