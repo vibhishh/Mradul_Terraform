474009be64429496326ea0d3a9766f94928fb533
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-12 01:28:43 -0500

    Update variables.tf

--- Terraform_Project/modules/files/variables.tf
+++ Terraform_Project/modules/files/variables.tf
@@ -1,29 +1,29 @@
 variable "ami_id" {
   description = "This is the AMI ID based on modules."
-  type = "S"
+  type = string
 }
 
 variable "instance_type" {
   description = "This is the Instance-Type based on modules."
-  type = "S"
+  type = string
 }
 
 variable "instance_name" {
   description = "This is the Instance-Name based on modules."
-  type = "S"
+  type = string
 }
 
 variable "bucket_name" {
   description = "This is the Bucket Name based on modules."
-  type = "S"
+  type = string
 }
 
 variable "table_name" {
   description = "This is the Table Name based on modules."
-  type = "S"
+  type = string
 }
 
 variable "env_name" {
   description = "This is the Environment Name based on modules."
-  type = "S"
+  type = string
 }
