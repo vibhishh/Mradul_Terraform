513327a30a27140d7efd6ae2f6583e09a17b73d8
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-16 11:19:19 +0530

    Create variables.tf

---
+++ RDS/variables.tf
@@ -0,0 +1,23 @@
+variable "rds_db_admin" {
+}
+
+variable "rds_db_password" {
+}
+
+variable "multi_az" {
+}
+
+variable "db_name" {
+}
+
+variable "engine_version" {
+}
+
+variable "allocated_storage" {
+}
+
+variable "instance_class" {
+}
+
+variable "db_engine" {
+}
