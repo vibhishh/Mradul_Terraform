42ea554e5426276fb72a20b2ec5e5f31a970b72f
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2023-10-16 11:36:43 +0530

    Update variables.tf

--- Terraform_Project/modules/files/variables.tf
+++ Terraform_Project/modules/files/variables.tf
@@ -31,31 +31,6 @@ variable "table_name" {
   type = string
 }
 
-#These variables are for RDS
-
-*/variable "rds_db_admin" {
-}
-
-variable "rds_db_password" {
-}
-
-variable "multi_az" {
-}
-
-variable "db_name" {
-}
-
-variable "engine_version" {
-}
-
-variable "allocated_storage" {
-}
-
-variable "instance_class" {
-}
-
-variable "db_engine" {
-}/*
 
 #ENV variable
 
