cad1a5694d5f1f9b8713b15ff7e2d74d471fba84
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-03-08 12:40:19 +0530

    Create main.tf

---
+++ Terraform_Project/modules/files/main.tf
@@ -0,0 +1,32 @@
+# Dev Infrastructure
+module "dev-demo" {
+  source = "./modules/files"
+  env_name = "DEV"
+  instance_type = "t2.micro"
+  ami_id = "ami-053b0d53c279acc90" #ubuntu
+  instance_name = "demo_instance"
+  bucket_name = "demo_bucckkeet"
+  table_name = "demo_table"
+}
+
+# QR Infrastructure
+module "qr-demo" {
+  source = "./modules/files"
+  env_name = "QR"
+  instance_type = "t2.small"
+  ami_id = "ami-041feb57c611358bd" #amazon-linux
+  instance_name = "demo_instance"
+  bucket_name = "demo_buckkettt"
+  table_name = "demo_table"
+}
+
+# PRD Infrastructure
+module "prd-demo" {
+  source = "./modules/files"
+  env_name = "PRD"
+  instance_type = "t2.medium"
+  ami_id = "ami-026ebd4cfe2c043b2" #redhat
+  instance_name = "demo_instance"
+  bucket_name = "demo_buckeett"
+  table_name = "demo_table"
+}
