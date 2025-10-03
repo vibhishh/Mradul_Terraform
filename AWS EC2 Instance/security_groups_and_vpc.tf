d46543934fcfa6b8b6c547db02bdfde7ba42bf51
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-03 16:10:58 +0530

    Update security_groups_and_vpc.tf

--- AWS EC2 Instance/security_groups_and_vpc.tf
+++ AWS EC2 Instance/security_groups_and_vpc.tf
@@ -1,23 +1,27 @@
-resource "aws_default_vpc" "default_vpc" {
-}
+resource "aws_default_vpc" "default" {}
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Example security group for EC2 instance"
+resource "aws_security_group" "my_sg" {
+  name   = "default-sg"
+  vpc_id = aws_default_vpc.default.id
 
-  # Define inbound and outbound rules for the security group
-  # Example: Allow SSH (port 22) and HTTP (port 80) traffic
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere (for demonstration purposes)
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
