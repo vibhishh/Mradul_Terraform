7c7cd97a1b5746695b9677d7f980ca1452c99fd5
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-04 14:51:47 +0530

    Update demo_ec2_full.tf

--- demo_ec2_full.tf
+++ demo_ec2_full.tf
@@ -22,22 +22,19 @@ resource "aws_security_group" "my_sg" {
   description = "Allow SSH & HTTP"
   vpc_id      = aws_default_vpc.default.id
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+ ingress = [
+    for port in [22, 80, 443, 8080, 9000,9100,9090,3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
   egress {
     from_port   = 0
     to_port     = 0
