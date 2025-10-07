07d543ea51b91c078b53fc882c6779c8d5d1910a
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-07 16:24:14 +0530

    Update private_ec2.tf

--- VPC/private_ec2.tf
+++ VPC/private_ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "public" {
+resource "aws_instance" "private" {
   subnet_id = aws_subnet.private-subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
   ami = "ami-0360c520857e3138f"
