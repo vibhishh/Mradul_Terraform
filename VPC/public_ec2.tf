3a0d5724812d75c003e927a089ffe2e83974de62
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-07 16:21:11 +0530

    Update public_ec2.tf

--- VPC/public_ec2.tf
+++ VPC/public_ec2.tf
@@ -3,7 +3,15 @@ resource "aws_instance" "public" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   ami = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+    delete_on_termination = true
+    tags = {
+      Name = "ec2-ebs-volume"
+    }}
   tags = {
     Name = "My-Pub-EC2"
   }
 }
+
