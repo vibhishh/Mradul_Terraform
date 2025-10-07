5aed19bee6158f8891f953956576747826fc444a
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-07 16:21:32 +0530

    Update private_ec2.tf

--- VPC/private_ec2.tf
+++ VPC/private_ec2.tf
@@ -3,6 +3,13 @@ resource "aws_instance" "private" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   ami = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+    delete_on_termination = true
+    tags = {
+      Name = "ec2-ebs-volume"
+    }}
   tags = {
     Name = "My-Pvt-EC2"
   }
