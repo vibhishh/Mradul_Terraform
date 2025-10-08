672ba6e0248ca0f64569d17a0e06a62e6cfa4add
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-08 09:17:36 +0530

    Update public_ec2.tf

--- VPC/public_ec2.tf
+++ VPC/public_ec2.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "public" {
   subnet_id = aws_subnet.pb-subnet.id
-  vpc_security_group_ids = [aws_security_group.sg.id]
   ami = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
   root_block_device {
@@ -15,3 +14,4 @@ resource "aws_instance" "public" {
     Name = "My-Pub-EC2"
   }
 }
+
