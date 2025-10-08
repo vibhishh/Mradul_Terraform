e9a4ba4ec4bb800fa7bc78dbc346204e78ac8f66
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-08 09:17:23 +0530

    Update private_ec2.tf

--- VPC/private_ec2.tf
+++ VPC/private_ec2.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "private" {
   subnet_id = aws_subnet.private-subnet.id
-  vpc_security_group_ids = [aws_security_group.sg.id]
   ami = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
   root_block_device {
